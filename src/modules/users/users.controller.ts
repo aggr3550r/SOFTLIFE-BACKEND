9f1ae35a6f890207b09611ee400c3c36eb698051
Victor Uche <ucheviquetor@gmail.com>
2022-04-28 00:12:33 +0100

    bug fixes

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -19,12 +19,12 @@ export class UsersController {
 
     @Get('/whoami')
     @UseGuards(AuthGuard)
-    whoAmI(@CurrentUser() user: User){
-         return user;
+    async whoAmI(@CurrentUser() user: User): Promise<User>{
+         return await user;
     }
 
     @Post('/signup')
-    async createUser(@Body() body: CreateUserDTO, @Session() session: any)
+    async createUser(@Body() body: CreateUserDTO, @Session() session: any): Promise<User>
     {
       const user = await this.authService.signup(body.email, body.password, body.username);
       session.userID = user.id;
@@ -32,41 +32,41 @@ export class UsersController {
     }
 
     @Post('/signin')
-    async signin(@Body() body: SignInDTO, @Session() session: any){
+    async signin(@Body() body: SignInDTO, @Session() session: any): Promise<User>{
         const user = await this.authService.signin(body.email, body.password);
         session.userID = user.id;
         return user;
     }
     
     @Post('/signout')
-    signOut(@Session() session: any){
+    signOut(@Session() session: any): void{
         session.userID = null;
     }
 
     @Get('/emails')
-     getAllRegisteredEmails() {
+     getAllRegisteredEmails(): Promise<User[]> {
          return this.usersService.findAllEmails();
      }
 
      @Get('/:id')
-     findUser(@Param('id') id){
+     findUser(@Param('id') id: string): Promise<User>{
          return this.usersService.findOne(id);
      }
 
      @Get()
-     findAllUsers(@Query('email') email: string) {
+     findAllUsers(@Query('email') email: string): Promise<User[]> {
          return this.usersService.find(email);
      }
 
 
      @Delete()
-     removeUser(@Query('id') id){
+     removeUser(@Query('id') id: string): Promise<User>{
          return this.usersService.remove(id);
      }
 
      @Patch('/:id')
-     updateUser(@Param('id') id: string, @Body() body: UpdateUserDTO){
-         return this.usersService.update(parseInt(id), body);
+     updateUser(@Param('id') id: string, @Body() body: UpdateUserDTO): Promise<User>{
+         return this.usersService.update(id, body);
      }
 }
             
--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -2,12 +2,12 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UpdateUserDTO } from './dtos/update-user.dto';
 import { UserRepository } from './repository/user.repository';
-
+import { User } from './entities/user.entity';
 @Injectable()
 export class UsersService {
     constructor(@InjectRepository(UserRepository) private userRepository: UserRepository){}
 
-    create(email: string, password:string, username: string){
+    create(email: string, password:string, username: string): Promise<User>{
         if(!email || !password){
             return null;
         }
@@ -15,30 +15,26 @@ export class UsersService {
         return this.userRepository.save(user);
     }
 
-    find(email: string) {
+    find(email: string): Promise<User[]> {
         if(!email){
             return null;
         }
         return this.userRepository.find({email});
     }
 
-    async findAllEmails() {
-        const users = await this.userRepository.find();
-        const emails = [];
-        users.forEach(e => {
-            emails.push(e.email);
-        });
-        return emails;
+    async findAllEmails(): Promise<User[]> {
+        const users_emails = await this.userRepository.find({select: ['email']});
+        return users_emails;
     }
 
-    findOne(id) {
+    findOne(id: string): Promise<User> {
         if(!id){
             return null;
         }
         return this.userRepository.findOne(id);
     }
 
-    async update(id: number, body: UpdateUserDTO){
+    async update(id: string, body: UpdateUserDTO): Promise<User>{
         const user = await this.userRepository.findOne(id);
         if (!user) {
             throw new NotFoundException("User not found");
@@ -47,7 +43,7 @@ export class UsersService {
         return this.userRepository.save(user); 
     }
 
-    async remove(id){
+    async remove(id: string): Promise<User>{
         const user = await this.userRepository.findOne(id);
         if(!user){
             throw new NotFoundException("User does not exist"); 
